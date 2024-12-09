0ed3cdf5db76c5ea207a6b22c611edbb36b77ef8
sravan <yenkeyyes4@gmail.com>
2024-12-09 10:03:04 +0530

    created output display area using swing jlabel class and added colors and required alignment for the better visibility and also attach this label to jframe

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,10 @@
+import java.awt.Color;
+import java.awt.Font;
+
+import javax.swing.BorderFactory;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 public class Calculator {
 	
@@ -8,7 +14,20 @@ public class Calculator {
 		jf.setLayout(null);
 		jf.setSize(1000,1200);
 		jf.setVisible(true);
+		jf.setLocation(200,250);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//outputdisplay to the user using jlabel class
+		JLabel displayBox=new JLabel("Hello");
+		displayBox.setBackground(Color.gray);
+		displayBox.setForeground(Color.white);
+		displayBox.setFont(new Font("Arial", Font.PLAIN, 90));
+		displayBox.setBorder(BorderFactory.createEmptyBorder(20, 15, 20, 15));
+		displayBox.setHorizontalAlignment(SwingConstants.RIGHT);
+		displayBox.setBounds(50,50,900,110);
+		displayBox.setOpaque(true);
+		//code to add display box to the JFrame
+		jf.add(displayBox);
 	}
 	
 	//main function starts here
