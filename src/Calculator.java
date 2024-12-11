cb6629cac33799654239b4a9724efea407ade7ce
sravan <yenkeyyes4@gmail.com>
2024-12-11 08:33:58 +0530

    created calculator digits one to six and aligned properly

--- src/Calculator.java
+++ src/Calculator.java
@@ -42,15 +42,60 @@ public class Calculator {
 		numberEight.setBounds(210, 210, 130, 130);
 		numberEight.setFont(new Font("Arial", Font.PLAIN, 90));
 		numberEight.setBorderPainted(false);
-		//code to add number seven to JFrame
+		//code to add number eight to JFrame
 		jf.add(numberEight);
 		
 		JButton numberNine=new JButton("9");
 		numberNine.setBounds(370, 210, 130, 130);
 		numberNine.setFont(new Font("Arial", Font.PLAIN, 90));
 		numberNine.setBorderPainted(false);
-		//code to add number seven to JFrame
+		//code to add number nine to JFrame
 		jf.add(numberNine);
+		
+		//digits of calulator 4-6
+		JButton numberFour=new JButton("4");
+		numberFour.setBounds(50, 370, 130, 130);
+		numberFour.setFont(new Font("Arial", Font.PLAIN, 90));
+		numberFour.setBorderPainted(false);
+		//code to add number four to JFrame
+		jf.add(numberFour);
+		
+		JButton numberFive=new JButton("5");
+		numberFive.setBounds(210, 370, 130, 130);
+		numberFive.setFont(new Font("Arial", Font.PLAIN, 90));
+		numberFive.setBorderPainted(false);
+		//code to add number five to JFrame
+		jf.add(numberFive);
+		
+		JButton numberSix=new JButton("6");
+		numberSix.setBounds(370, 370, 130, 130);
+		numberSix.setFont(new Font("Arial", Font.PLAIN, 90));
+		numberSix.setBorderPainted(false);
+		//code to add number six to JFrame
+		jf.add(numberSix);
+		
+		
+		//digits of calulator 1-3
+		JButton numberOne=new JButton("1");
+		numberOne.setBounds(50, 530, 130, 130);
+		numberOne.setFont(new Font("Arial", Font.PLAIN, 90));
+		numberOne.setBorderPainted(false);
+		//code to add number One to JFrame
+		jf.add(numberOne);
+		
+		JButton numberTwo=new JButton("2");
+		numberTwo.setBounds(210, 530, 130, 130);
+		numberTwo.setFont(new Font("Arial", Font.PLAIN, 90));
+		numberTwo.setBorderPainted(false);
+		//code to add number Two to JFrame
+		jf.add(numberTwo);
+		
+		JButton numberThree=new JButton("3");
+		numberThree.setBounds(370, 530, 130, 130);
+		numberThree.setFont(new Font("Arial", Font.PLAIN, 90));
+		numberThree.setBorderPainted(false);
+		//code to add number Three to JFrame
+		jf.add(numberThree);
 	}
 	
 	//main function starts here
