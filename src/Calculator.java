fe3a577121b80580d87e5de654876c2a9a1a8f80
sravan <yenkeyyes4@gmail.com>
2024-12-12 11:29:42 +0530

    created backspace and clear button and given proper action to both buttons and also done the statements to if the operator clicked what to happen to the numbers

--- src/Calculator.java
+++ src/Calculator.java
@@ -10,15 +10,20 @@ import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 
 public class Calculator implements ActionListener {
+	//cheking any operator clicked?
+	boolean isOperatorClicked=false;
 	JLabel displayBox;
+	//digits obects
 	JButton numberSeven,numberEight,numberNine,numberFour,numberFive,
-	numberSix,numberOne,numberTwo;,numberThree,dotButton,numberZero;
+	numberSix,numberOne,numberTwo,numberThree,dotButton,numberZero;
+	//operators objects
+	JButton divisionButton,additionButton,subtractionButton,multiplictionButton,clearButton,backSpace;
 	
 	//initialize jframe class to create window to run calculator
 	Calculator(){
 		JFrame jf=new JFrame("Calculator");
 		jf.setLayout(null);
-		jf.setSize(720,1000);
+		jf.setSize(720,1100);
 		jf.setVisible(true);
 		jf.setLocation(200,250);
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -138,33 +143,51 @@ public class Calculator implements ActionListener {
 		
 		
 		//digits of operations
-		JButton divisionButton=new JButton("/");
+		divisionButton=new JButton("/");
 		divisionButton.setBounds(530, 210, 130, 130);
 		divisionButton.setFont(new Font("Arial", Font.PLAIN, 90));
 		divisionButton.setBorderPainted(false);
+		divisionButton.addActionListener(this);
 		//code to add division button to JFrame
 		jf.add(divisionButton);
 		
-		JButton additionButton=new JButton("+");
+		additionButton=new JButton("+");
 		additionButton.setBounds(530, 370, 130, 130);
 		additionButton.setFont(new Font("Arial", Font.PLAIN, 90));
 		additionButton.setBorderPainted(false);
+		additionButton.addActionListener(this);
 		//code to add addition button to JFrame
 		jf.add(additionButton);
 		
-		JButton subtractionButton=new JButton("-");
+		subtractionButton=new JButton("-");
 		subtractionButton.setBounds(530, 530, 130, 130);
 		subtractionButton.setFont(new Font("Arial", Font.PLAIN, 90));
 		subtractionButton.setBorderPainted(false);
+		subtractionButton.addActionListener(this);
 		//code to add subtraction button to JFrame
 		jf.add(subtractionButton);
 		
-		JButton multiplictionButton=new JButton("*");
+		multiplictionButton=new JButton("*");
 		multiplictionButton.setBounds(530, 690, 130, 130);
 		multiplictionButton.setFont(new Font("Arial", Font.PLAIN, 90));
 		multiplictionButton.setBorderPainted(false);
+		multiplictionButton.addActionListener(this);
 		//code to add subtraction button to JFrame
 		jf.add(multiplictionButton);
+		
+		clearButton=new JButton("Clear");
+		clearButton.setBounds(50, 850, 290, 130);
+		clearButton.setFont(new Font("Arial", Font.PLAIN, 40));
+		clearButton.setBorderPainted(false);
+		clearButton.addActionListener(this);
+		jf.add(clearButton);
+		
+		backSpace=new JButton("Backspace");
+		backSpace.setBounds(370, 850, 290, 130);
+		backSpace.setFont(new Font("Arial", Font.PLAIN, 40));
+		backSpace.setBorderPainted(false);
+		backSpace.addActionListener(this);
+		jf.add(backSpace);
 	}
 	
 	//main function starts here
@@ -175,42 +198,131 @@ public class Calculator implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource()==numberSeven) {
-			displayBox.setText(displayBox.getText()+"7");
+			if(isOperatorClicked) {
+				displayBox.setText("7");
+				isOperatorClicked=false;
+			}
+			else {
+				displayBox.setText(displayBox.getText()+"7");
+			}
 		}
 		else if(e.getSource()==numberEight) {
-			displayBox.setText(displayBox.getText()+"8");
+				if(isOperatorClicked) {
+					displayBox.setText("8");
+					isOperatorClicked=false;
+				}
+				else {
+					displayBox.setText(displayBox.getText()+"8");
+				}
 		}
 		else if(e.getSource()==numberNine) {
-			displayBox.setText(displayBox.getText()+"9");
+				if(isOperatorClicked) {
+					displayBox.setText("9");
+					isOperatorClicked=false;
+				}
+				else {
+					displayBox.setText(displayBox.getText()+"9");
+				}
 		}
 		else if(e.getSource()==numberSix) {
-			displayBox.setText(displayBox.getText()+"6");
+				if(isOperatorClicked) {
+					displayBox.setText("6");
+					isOperatorClicked=false;
+				}
+				else {
+					displayBox.setText(displayBox.getText()+"6");
+				}
 		}
 		else if(e.getSource()==numberFive) {
-			displayBox.setText(displayBox.getText()+"5");
+				if(isOperatorClicked) {
+					displayBox.setText("5");
+					isOperatorClicked=false;
+				}
+				else {
+					displayBox.setText(displayBox.getText()+"5");
+				}
 		}
 		else if(e.getSource()==numberFour) {
-			displayBox.setText(displayBox.getText()+"4");
+				if(isOperatorClicked) {
+					displayBox.setText("4");
+					isOperatorClicked=false;
+				}
+				else {
+					displayBox.setText(displayBox.getText()+"4");
+				}
 		}
 		else if(e.getSource()==numberThree) {
-			displayBox.setText(displayBox.getText()+"3");
+				if(isOperatorClicked) {
+					displayBox.setText("3");
+					isOperatorClicked=false;
+				}
+				else {
+					displayBox.setText(displayBox.getText()+"3");
+				}
 		}
 		else if(e.getSource()==numberTwo) {
-			displayBox.setText(displayBox.getText()+"2");
+				if(isOperatorClicked) {
+					displayBox.setText("2");
+					isOperatorClicked=false;
+				}
+				else {
+					displayBox.setText(displayBox.getText()+"2");
+				}
 		}
 		else if(e.getSource()==numberOne) {
-			displayBox.setText(displayBox.getText()+"1");
+				if(isOperatorClicked) {
+					displayBox.setText("1");
+					isOperatorClicked=false;
+				}
+				else {
+					displayBox.setText(displayBox.getText()+"1");
+				}
 		}
 		else if(e.getSource()==numberZero) {
-			displayBox.setText(displayBox.getText()+"0");
+				if(isOperatorClicked) {
+					displayBox.setText("0");
+					isOperatorClicked=false;
+				}
+				else {
+					displayBox.setText(displayBox.getText()+"0");
+				}
 		}
 		else if(e.getSource()==dotButton) {
-			displayBox.setText(displayBox.getText()+".");
+				if(isOperatorClicked) {
+					displayBox.setText(".");
+					isOperatorClicked=false;
+				}
+				else {
+					displayBox.setText(displayBox.getText()+".");
+				}
 		}
-		else if(e.getSource()==numberZero) {
-			displayBox.setText("0");
+		else if(e.getSource()==clearButton) {
+			displayBox.setText("");
+		}
+		else if(e.getSource()==backSpace) {
+			String currentText = displayBox.getText();
+		    if (!currentText.isEmpty()) {
+		        // Remove the last character
+		        displayBox.setText(currentText.substring(0, currentText.length() - 1));
+		    }
 		}
 		
+		//operations
+		else if(e.getSource()==additionButton) {
+			isOperatorClicked=true;
+		}
+		else if(e.getSource()==subtractionButton) {
+			isOperatorClicked=true;
+			
+		}
+		else if(e.getSource()==multiplictionButton) {
+			isOperatorClicked=true;
+			
+		}
+		else if(e.getSource()==divisionButton) {
+			isOperatorClicked=true;
+			
+		}
 		
 	}
 }
